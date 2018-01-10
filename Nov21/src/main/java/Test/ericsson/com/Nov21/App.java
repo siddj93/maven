17a539d639aad4d77404773285a32c20518f794e
Sid <siddj93@gmail.com>
2018-01-10 14:33:09 +0530

    maven demo

---
+++ Nov21/src/main/java/Test/ericsson/com/Nov21/App.java
@@ -0,0 +1,13 @@
+package Test.ericsson.com.Nov21;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
